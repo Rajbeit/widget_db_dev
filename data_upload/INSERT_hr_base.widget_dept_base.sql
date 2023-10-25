cc335e3543506a9dca6db17cc6343343875949d7
Rajbeit <148898964+Rajbeit@users.noreply.github.com>
2023-10-25 15:40:28 +0100

    Create INSERT_hr_base.widget_dept_base.sql
    Upload record for department

---
+++ data_upload/INSERT_hr_base.widget_dept_base.sql
@@ -0,0 +1,7 @@
+-- insert data into Department Base Table
+INSERT INTO hr_base.widget_dept_base (dept_id, dept_name, dept_loc) VALUES (1, 'Management', 'London');
+INSERT INTO hr_base.widget_dept_base (dept_id, dept_name, dept_loc) VALUES (2, 'Engineering', 'Cardiff');
+INSERT INTO hr_base.widget_dept_base (dept_id, dept_name, dept_loc) VALUES (3, 'Research & Development', 'Edinburgh');
+INSERT INTO hr_base.widget_dept_base (dept_id, dept_name, dept_loc) VALUES (4, 'Sales', 'Belfast');
+
+commit;
